26c572fe900d6db2f61d7a6653677d2270780d98
0xGSAT <0xGSAT@gmail.com>
2023-05-30 11:00:25 -0700

    diabled button and refactored guards

--- angularPuzzle/src/guards/admin.guard.ts
+++ angularPuzzle/src/guards/admin.guard.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
 import { CurrentUserService } from 'src/services/current-user.service';
-import { AuthenticationService } from '../../services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuard implements CanActivate {
+
+export class AdminGuard implements CanActivate {
   constructor(private currentUser: CurrentUserService, private router: Router) {}
 
   canActivate(): boolean {
-    if (this.currentUser.user.role === 'user') {
+    if (this.currentUser.user.role === 'admin') {
       return true; 
     } else {
       this.router.navigate(['/login']); 
       return false;
     }
   }
-  
 }
--- angularPuzzle/src/guards/user.guard.ts
+++ angularPuzzle/src/guards/user.guard.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router, } from '@angular/router';
 import { CurrentUserService } from 'src/services/current-user.service';
-import { AuthenticationService } from '../../services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-
-export class AdminGuard implements CanActivate {
+export class UserGuard implements CanActivate {
   constructor(private currentUser: CurrentUserService, private router: Router) {}
 
   canActivate(): boolean {
-    if (this.currentUser.user.role === 'admin') {
+    if (this.currentUser.user.role === 'user') {
       return true; 
     } else {
       this.router.navigate(['/login']); 
       return false;
     }
   }
+  
 }
